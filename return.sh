a78270826c40ce8b4635404d2898aea7b254b263
Felipe-noob <felipelorenzzon@gmail.com>
2025-08-14 17:31:44 +0200

    sh instead of zsh

--- return.sh
+++ return.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/zsh
+#!/usr/bin/env sh
 
 return_date=$1
 destination="PARIS (intramuros)"
@@ -6,5 +6,5 @@ destination="PARIS (intramuros)"
 file_name="$destination-$return_date.txt"
 
 while IFS= read -r line; do
-  python main.py -d $line -a a -q -p -t $return_date | grep $destination | tee --append "$file_name"
+  python main.py -d "$line" -a a -q -p -t $return_date | grep "$destination" | tee --append "$file_name"
 done < destinaisons.txt
