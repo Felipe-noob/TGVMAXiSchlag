ab88c47d767f46b1058dfbad6310c139880c7e7d
Felipe-noob <felipelorenzzon@gmail.com>
2025-08-16 10:02:32 +0200

    better file name

--- return.sh
+++ return.sh
@@ -3,7 +3,8 @@
 return_date=$1
 destination="PARIS (intramuros)"
 
-file_name="$destination-$return_date.txt"
+destination_sed=$(echo $destination | sed 's/ /_/g')
+file_name="output/$destination_sed-$return_date.txt"
 
 while IFS= read -r line; do
   python main.py -d "$line" -a a -q -p -t $return_date | grep "$destination" | tee --append "$file_name"
