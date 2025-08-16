1686d78c09ac685a870199a7c6c859c27c23b437
Felipe-noob <felipelorenzzon@gmail.com>
2025-08-16 21:47:23 +0200

    sort and filter my list of garez

--- return.sh
+++ return.sh
@@ -5,16 +5,17 @@ destination="PARIS (intramuros)"
 
 destination_sed=$(echo $destination | sed 's/ /_/g')
 file_name="output/$destination_sed-$return_date.txt"
+rm "$file_name"
 
 while IFS= read -r line; do
   python main.py \
     -d "$line" -a "$destination" \
     -s 1 -q -t $return_date \
-      | tee "$file_name";
+      | tee --append "$file_name";
 done < destinaisons.txt
 
 return
 tmp_file=$(mktemp)
-grep Depart "$file_name" > "$tmp_file"
+grep Départ "$file_name" > "$tmp_file"
 mv "$tmp_file" "$file_name"
 
