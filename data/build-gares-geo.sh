7096b2f93fee9c32bec6c962725470d2fbe0e255
Felipe-noob <felipelorenzzon@gmail.com>
2025-08-18 23:30:13 +0200

    plotting a large number of gares

--- data/build-gares-geo.sh
+++ data/build-gares-geo.sh
@@ -2,15 +2,17 @@
 
 # set -e
 
-echo "Address,Lat,Long" > gares-geo.csv
+echo "Address,Long,Lat" > gares-geo.csv
 
 while IFS= read -r line; do
+  line_filtered=$(echo $line | sed 's/ VILLE//')
+
   coordinates=$( \
-    grep -m 1 -i "$line" liste-des-gares.simple.csv \
+    grep -m 1 -i "$line_filtered" liste-des-gares.simple.csv \
     | cut --delimiter ";" --fields 4,5 | tr ";" "," \
   )
 
-  if [[ $coordinates == "" ]]; 
+  if [[ $coordinates == "" ]];
     then echo ERROR: $line;
   else
     echo $line,$coordinates | tee --append gares-geo.csv
