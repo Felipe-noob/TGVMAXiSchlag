214e9ed8d3989b6202db8bb088585bfb7b7aaaf3
Felipe-noob <felipelorenzzon@gmail.com>
2025-08-19 21:08:19 +0200

    in parallel

--- return.sh
+++ return.sh
@@ -9,12 +9,27 @@ destination_sed=$(echo $destination | sed 's/ /_/g')
 file_name="output/$destination_sed-$return_date.txt"
 rm -v -f "$file_name"
 
+max_jobs=24
+jobs=0
+
 while IFS= read -r line; do
+  if [[ $jobs -eq $max_jobs ]]; then
+    wait
+  fi
+
+  jobs=$(( jobs + 1 ))
   python main.py \
     -d "$line" -a "$destination" \
     -s 1 -q -t $return_date \
-      | tee --append "$file_name";
+      | tee --append "$file_name" && jobs=$(( jobs - 1 )) &
+
 done < destinaisons.txt
+
+wait
+
+echo
+echo Finished fetching
+echo
 
 # create a more readable file only with the names
 tmp_file=$(mktemp)
